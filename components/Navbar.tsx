1afd3153d5cd7f491f7a2939fe67d80e13760e3b
Ol-create <oluola96@gmail.com>
2024-11-21 21:09:50 +0100

    add mobile navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from 'react';
 
 import Link from 'next/link';
-import Image from 'next/image'
+import Image from 'next/image';
+
+import MobileNav from './MobileNav';
 
 const Navbar = () => {
   return (
-    <nav className='flex-between fixed z-50 w-full bg-dark-1 px-6 py-4 lg:px-10'>
+    <nav className=' flex flex-between fixed z-50 w-full
+              bg-dark-1 px-6 py-4 lg:px-10'>
       <Link href="/" className='flex items-center gap-1'>
       <Image 
         src="/icons/logo.svg"
@@ -16,6 +19,10 @@ const Navbar = () => {
         />
         <p className='text-[26px] font-extrabold text-white max-sm:hidden'>Yoom</p>
       </Link>
+      <div className='flex-between gap-5'>
+        {/* Clerk - User Management */}
+        <MobileNav />
+      </div>
     </nav>
   )
 }
